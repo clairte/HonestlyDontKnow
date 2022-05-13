bdab743aaa264e9486f7ba6f6cc933b9e2a3f65a
Claire Xu <claireaxu@g.ucla.edu>
2022-05-12 19:26:39 -0700

    thomas commit

---
+++ dininghall-enhanced/src/Window.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Window(){
+    let test = 10; 
+    return(
+        <>
+        <p> Dining Hall {test} </p>
+        <a> De Neve </a>
+        </>
+        
+    ); 
+}
+
+export default Window; 
