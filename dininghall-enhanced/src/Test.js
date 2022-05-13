6df7c72f397abc2013e0178c8aed9b204f4e4343
Vanessa <vanessajiang132@gmail.com>
2022-05-12 19:38:25 -0700

    commit Vanessa

---
+++ dininghall-enhanced/src/Test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+function Test() {
+    return(
+        <>
+        <input>
+        <h1> UCLA Dining Hall Menu </h1> 
+
+        </input>
+        </>
+
+    )   
+}
+  
+export default Test
+
